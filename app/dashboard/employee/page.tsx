7f9049e1031c7808fb9736845dc00863162c4687
Diego Letelier <diegoleteliers10@gmail.com>
2025-10-12 23:55:51 -0300

    Add Employee Dashboard components including EmployeeHome and HomeContent for job applications and alerts

---
+++ app/dashboard/employee/page.tsx
@@ -0,0 +1,12 @@
+import { DashboardLayout } from "@/components/Layouts/DashboardLayout";
+import { HomeContent } from "@/components/DashboardComponents/EmployeeDashboard/homeContent";
+
+const EmployeeHome = () => {
+  return (
+    <DashboardLayout>
+      <HomeContent />
+    </DashboardLayout>
+  );
+};
+
+export default EmployeeHome;
