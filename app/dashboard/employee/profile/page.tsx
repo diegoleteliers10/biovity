94d499702dfa3f7bb66fc0d13077afb35aa5a9b5
Diego Letelier <diegoleteliers10@gmail.com>
2025-10-26 18:31:15 -0300

    Add animated Radix DropdownMenu and useDataState
    Add basic employee profile page placeholder

---
+++ app/dashboard/employee/profile/page.tsx
@@ -0,0 +1,14 @@
+import { DashboardLayout } from "@/components/Layouts/DashboardLayout"
+
+const EmployeeProfile = () => {
+  return (
+    <DashboardLayout>
+      <div>
+        <p>Hola este es el profile</p>
+      </div>
+      {/*<ProfileContent />*/}
+    </DashboardLayout>
+  )
+};
+
+export default EmployeeProfile;
